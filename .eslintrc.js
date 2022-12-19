135908652ed5b842af93266c084703318ef45233
widcraft <forsythia3240@gmail.com>
2022-12-19 22:59:23 +0900

    [FIX] eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { es2021: true },
+  env: { es2021: true, node: true },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   overrides: [],
   parser: '@typescript-eslint/parser',
